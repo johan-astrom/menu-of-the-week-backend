a48be0ebaca60f7c5e1edc9588ebcd41327a9c88
Johan Åström <johan.astrom89@gmail.com>
2021-06-01 14:07:33 +0200

    GET and POST to recipes working

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const app = require('express')();
 const db = require('./db');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`server running on port: ${port}`));
 
+db.createDatabase();
+
 app.get('/recipes', (req, res) => {
-    db.query('SELECT * FROM recipes', [], (rows, err) => {
+    db.query('SELECT * FROM recipes;', [], (err, result) => {
         if (err) {
             res.status(400).json({
                 'error': err.message
@@ -14,7 +20,7 @@ app.get('/recipes', (req, res) => {
         } else {
             res.json({
                 'message': 'success',
-                'recipes': rows
+                'recipes': result.rows
             });
         }
     });
@@ -25,9 +31,9 @@ app.post('/recipes', (req, res) => {
         title: req.body.title,
         weekday: req.body.weekday
     }
-    let text = 'INSERT INTO recipes VALUES ($1, $2) RETURNING id;'
+    let text = 'INSERT INTO recipes (title, weekday) VALUES ($1, $2) RETURNING id;'
     let params = [data.title, data.weekday]
-    db.query(text, params, (result, err) => {
+    db.query(text, params, (err, result) => {
         if (err){
             res.status(400).json({
                 'error': err.message
--- db.js
+++ db.js
@@ -1,18 +1,31 @@
 const {Pool} = require('pg');
+const dotEnv = require('dotenv');
 
-const pool = new Pool;
+dotEnv.config();
 
-module.exports={
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+module.exports = {
     query: (text, params, callback) => {
         return pool.query(text, params, callback);
     },
     createDatabase: () => {
-        pool.query("CREATE DATABASE menu_of_the_week", [], err => console.log("DB menu_of_the_week found"));
-        pool.query("CREATE TABLE IF NOT EXISTS recipes (id SERIAL PRIMARY KEY, title TEXT, weekday VARCHAR(9)",
-            [], err => (console.log(err)));
+        pool.query("CREATE TABLE IF NOT EXISTS recipes (id SERIAL PRIMARY KEY, title TEXT, weekday VARCHAR(9))",
+            [], err => {
+                if (err) {
+                    console.log(err)
+                }
+            });
         pool.query("CREATE TABLE IF NOT EXISTS ingredients " +
-            "(id SERIAL PRIMARY KEY, name TEXT, amount SMALLINT, measurement TEXT",
-            [], err => (console.log(err)))
+            "(id SERIAL PRIMARY KEY, name TEXT, amount SMALLINT, measurement TEXT)",
+            [], err => {
+                if (err) {
+                    console.log(err)
+                }
+            });
     }
 }
 
